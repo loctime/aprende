627bd16eac9242445cf36e3281939ed0f27c8eb1
loctime <diegobertosi@gmail.com>
2026-05-15 00:06:54 -0300

    feat(mdx): wire custom components into MDX pipeline

---
+++ components/mdx/MDXComponents.tsx
@@ -0,0 +1,13 @@
+import { Ejercicio } from './Ejercicio'
+import { Solucion } from './Solucion'
+import { Ejemplo } from './Ejemplo'
+import { Definicion } from './Definicion'
+import { Diagrama } from './Diagrama'
+
+export const mdxComponents = {
+  Ejercicio,
+  Solucion,
+  Ejemplo,
+  Definicion,
+  Diagrama,
+}
